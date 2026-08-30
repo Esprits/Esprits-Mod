5f773a58870a3a319d55ede21017c393e68b0ac1
Esprits <lmignacca@outlook.com>
2026-08-30 19:50:50 -0400

    Initial Commit - Created Fabric initial files

---
+++ src/client/java/espritsmod/client/EspritsModDataGenerator.java
@@ -0,0 +1,11 @@
+package espritsmod.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EspritsModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
