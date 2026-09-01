5233b6995e2e748a668ff52b535440de45d392e9
Esprits <lmignacca@outlook.com>
2026-09-01 02:20:42 -0400

    Added Tree Bark - Added esprits-mod:tree_bark item - Added tree_bark.png texture

--- src/main/java/espritsmod/references/ModItemIDs.java
+++ src/main/java/espritsmod/references/ModItemIDs.java
@@ -7,6 +7,8 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.Item;
 
 public class ModItemIDs {
+    public static final ResourceKey<Item> TREE_BARK = create("tree_bark");
+
     public static ResourceKey<Item> create(String name) {
         return ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(EspritsMod.MOD_ID, name));
     }
--- src/main/java/espritsmod/world/item/ModItems.java
+++ src/main/java/espritsmod/world/item/ModItems.java
@@ -1,13 +1,18 @@
 package espritsmod.world.item;
 
+import espritsmod.references.ModItemIDs;
+import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 
 import java.util.function.Function;
 
 public class ModItems {
+    public static final Item TREE_BARK = register(ModItemIDs.TREE_BARK, Item::new, new Item.Properties());
+
     public static Item register(ResourceKey<Item> itemKey, Function<Item.Properties, Item> itemFactory, Item.Properties settings) {
         // Create the item instance
         Item item = itemFactory.apply(settings.setId(itemKey));
@@ -19,5 +24,8 @@ public class ModItems {
     }
 
     public static void initialize() {
+        // Get the event for modifying entries in the ingredients group
+        // and register an event handler that adds tree bark to the ingredients group
+        CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.INGREDIENTS).register((creativeTab) -> creativeTab.accept(ModItems.TREE_BARK));
     }
 }
