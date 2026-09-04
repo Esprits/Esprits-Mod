b39713e43cba5c9c80537a04ee147fd94449f479
Esprits <lmignacca@outlook.com>
2026-09-03 22:56:35 -0400

    Adjusted the wooden armour craft - It now includes all tree barks

--- src/client/java/esprits_mod/client/datagen/ModRecipeProvider.java
+++ src/client/java/esprits_mod/client/datagen/ModRecipeProvider.java
@@ -1,5 +1,6 @@
 package esprits_mod.client.datagen;
 
+import esprits_mod.tags.ModItemTags;
 import esprits_mod.world.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
@@ -8,6 +9,7 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 
 import java.util.concurrent.CompletableFuture;
@@ -27,31 +29,35 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 shaped(RecipeCategory.COMBAT, ModItems.WOOD_HELMET, 1)
                         .pattern("XXX")
                         .pattern("X X")
-                        .define('X', ModItems.OAK_BARK)
-                        .unlockedBy(getHasName(ModItems.OAK_BARK), has(ModItems.OAK_BARK))
+                        .define('X', ModItemTags.TREE_BARK)
+                        .unlockedBy(getHasNameTag(ModItemTags.TREE_BARK), has(ModItemTags.TREE_BARK))
                         .save(output);
                 shaped(RecipeCategory.COMBAT, ModItems.WOOD_CHESTPLATE, 1)
                         .pattern("X X")
                         .pattern("XXX")
                         .pattern("XXX")
-                        .define('X', ModItems.OAK_BARK)
-                        .unlockedBy(getHasName(ModItems.OAK_BARK), has(ModItems.OAK_BARK))
+                        .define('X', ModItemTags.TREE_BARK)
+                        .unlockedBy(getHasNameTag(ModItemTags.TREE_BARK), has(ModItemTags.TREE_BARK))
                         .save(output);
                 shaped(RecipeCategory.COMBAT, ModItems.WOOD_LEGGINGS, 1)
                         .pattern("XXX")
                         .pattern("X X")
                         .pattern("X X")
-                        .define('X', ModItems.OAK_BARK)
-                        .unlockedBy(getHasName(ModItems.OAK_BARK), has(ModItems.OAK_BARK))
+                        .define('X', ModItemTags.TREE_BARK)
+                        .unlockedBy(getHasNameTag(ModItemTags.TREE_BARK), has(ModItemTags.TREE_BARK))
                         .save(output);
                 shaped(RecipeCategory.COMBAT, ModItems.WOOD_BOOTS, 1)
                         .pattern("X X")
                         .pattern("X X")
-                        .define('X', ModItems.OAK_BARK)
-                        .unlockedBy(getHasName(ModItems.OAK_BARK), has(ModItems.OAK_BARK))
+                        .define('X', ModItemTags.TREE_BARK)
+                        .unlockedBy(getHasNameTag(ModItemTags.TREE_BARK), has(ModItemTags.TREE_BARK))
                         .save(output);
             }
         };
+    }
+
+    protected String getHasNameTag(TagKey<Item> tag) {
+        return "has_" + tag.location().getPath();
     }
 
     @Override
